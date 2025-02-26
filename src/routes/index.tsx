c805aefd8272dcf73753897244dfd64c835fc172
Charles Dong <chardon_cs@proton.me>
2025-02-26 15:16:39 +0800

    Move head object to the top

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,16 @@
 import { component$ } from "@builder.io/qwik"
 import type { DocumentHead } from "@builder.io/qwik-city"
 
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+}
+
 export default component$(() => {
   return (
     <>
@@ -13,13 +23,3 @@ export default component$(() => {
     </>
   )
 })
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-}
