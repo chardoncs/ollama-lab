96bf3d336febc2da49a997ae3daeaab3b4c5c767
Charles Dong <chardon_cs@proton.me>
2025-02-27 18:38:53 +0800

    Add loader icon

--- src/integrations/react/lucide-icons.tsx
+++ src/integrations/react/lucide-icons.tsx
@@ -1,8 +1,14 @@
 /** @jsxImportSource react */
 
 import { qwikify$ } from "@builder.io/qwik-react"
-import { BotMessageSquare, Package, Settings } from "lucide-react"
+import {
+  BotMessageSquare,
+  Loader2,
+  Package,
+  Settings,
+} from "lucide-react"
 
 export const BotMessageSquareIcon = qwikify$(BotMessageSquare)
+export const Loader2Icon = qwikify$(Loader2)
 export const PackageIcon = qwikify$(Package)
 export const SettingsIcon = qwikify$(Settings)
